735e7fade8d7563c739c90dc14d21f6f03d02ee7
Şuayip Işık <suayip.isik.16@gmail.com>
2023-11-19 15:40:01 +0300

    SignUpScreen ui fix

--- src/pages/SignUpScreen.tsx
+++ src/pages/SignUpScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
 import { useGlobalStyles } from "../hooks";
 import { CustomButton, CustomTextInput } from "../components";
