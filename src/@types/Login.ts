d3b6c385f9307620825918b3b7f19947647676d0
Şuayip Işık <suayip.isik.16@gmail.com>
2023-11-19 22:33:20 +0300

    fix

--- src/@types/Login.ts
+++ src/@types/Login.ts
@@ -2,11 +2,3 @@ export interface MyLoginFormValues {
   userName: string;
   password: string;
 }
-export interface MySignUpFormValues {
-  name: string;
-  surName: string;
-  age: string;
-  eMail: string;
-  userName: string;
-  password: string;
-}
---
+++ src/@types/SignUp.ts
@@ -0,0 +1,8 @@
+export interface MySignUpFormValues {
+  name: string;
+  surName: string;
+  age: string;
+  eMail: string;
+  userName: string;
+  password: string;
+}
